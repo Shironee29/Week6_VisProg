6841e483fa307a45d0b528e444ba2560c2a41e08
stanleyy30 <stanleyyoung300705@gmail.com>
2024-10-22 11:42:35 +0700

    Commitment

--- app/src/main/java/com/example/week6/BaruBro.kt
+++ app/src/main/java/com/example/week6/BaruBro.kt
@@ -2,18 +2,11 @@ package com.example.week6
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-public fun main(){
+fun Mainmain(){
     Text(
         "Hello"
     )
     Text("Ezzzzzz Browwww")
-}
-
-@Preview
-@Composable
-fun mainPreview(){
-    main()
 }
