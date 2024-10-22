d6bff3187ef1e48a7c48aa2c9e9e5da5ac088719
stanleyy30 <stanleyyoung300705@gmail.com>
2024-10-22 11:31:19 +0700

    New Updates

--- app/src/main/java/com/example/week6/BaruBro.kt
+++ app/src/main/java/com/example/week6/BaruBro.kt
@@ -1,5 +1,18 @@
 package com.example.week6
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
+public fun main(){
+    Text(
+        "Hello"
+    )
+}
+
+@Preview
+@Composable
+fun mainPreview(){
+    main()
+}
